a00817a212fa456bd00cc66226d8d9cd2f65801c
FREDSON SILVA <silva.fredson@hotmail.com>
2024-04-15 14:23:27 -0300

    Initial Commit

---
+++ inchurch-ios-challenge/inchurch_ios_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  inchurch_ios_challengeApp.swift
+//  inchurch-ios-challenge
+//
+//  Created by Fredson Silva on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct inchurch_ios_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
